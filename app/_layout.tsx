44509faf2eb005a549da61f9b8ad41089a8e3290
davidvarver <davidvarver@gmail.com>
2026-01-11 01:59:53 -0600

    fix: unmatched route by refactoring imports and touching layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
+// Force router refresh
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
